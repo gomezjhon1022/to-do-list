743d0f9a14a8f3e4447976dec385eff5f370d4d1
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-08-11 16:46:05 -0500

    Maquetación y estilos de los componentes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch.js";
+import { TodoList } from "./TodoList.js";
+import { TodoItem } from "./TodoItem.js";
+import { CreateTodoButton } from "./CreateTodoButton.js";
+
+const todos = [
+  {text: 'Cortar cereza', completed: false},
+  {text: 'Rodar en moto', completed: true},
+  {text: 'Hacer ejercicio', completed: false}
+
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    <TodoCounter />
+    <TodoSearch />
+    <TodoList>
+      {todos.map(todo => (
+        <TodoItem
+          key={todo.text}
+          text={todo.text}
+          completed={todo.completed}
+        />
+      ))}
+    </TodoList>
+    <CreateTodoButton />
+  </React.Fragment>
   );
 }
 
